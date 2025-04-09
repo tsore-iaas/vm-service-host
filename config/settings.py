ca780823f2a8a80a5cfe580bd44c002802b5fb3e
Tchassi Daniel <tchassidaniel@gmail.com>
2025-04-09 15:25:29 +0100

    script to install

--- config/settings.py
+++ config/settings.py
@@ -1,19 +1,22 @@
+import os
+
 #Master configuration
-MASTER_IP = "192.168.1.200"
-MASTER_PORT = "8000"
+MASTER_IP = os.environ.get('MASTER_IP', '192.168.8.163')
+MASTER_PORT = os.environ.get('MASTER_PORT', '8000')
 
 #Host Configuration
-HOST_IP = "192.168.1.143"
-LOCATION = "Cameroun"
+HOST_IP = os.environ.get('HOST_IP', '192.168.8.163')
+LOCATION = os.environ.get('LOCATION', 'Cameroun')
+
 
 #Network Configuration
-HOST_INTERFACE_NAME = "eno1" # Nom de l'interface réseau sur l'hôte pour permettre au VM d'accéder au réseau
+HOST_INTERFACE_NAME = os.environ.get('HOST_INTERFACE_NAME', 'eno1') # Nom de l'interface réseau sur l'hôte pour permettre au VM d'accéder au réseau
 
 #VM Configuration
 TMP_DIR = "/tmp/firecracker_sockets"
 BASE_DIR = "/tmp/vms"
 SOCKET_PREFIX = "firecracker"
 BRIDGE_NAME = "br0"
-GATEWAY_ADDRESS = "192.168.1.1"
+GATEWAY_ADDRESS = os.environ.get('GATEWAY_ADDRESS', '192.168.8.1')
 
 SCRIPTS_FOLDER = "app/scripts/"
